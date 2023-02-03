389953159351174adc6c942ad8f10e6fa0bb96f7
alijavaboy <ali77erfagh99@gmail.com>
2023-02-03 14:06:35 +0330

    pass all test

--- src/main/java/com/codeWars/reversesWords/ReverseWords.java
+++ src/main/java/com/codeWars/reversesWords/ReverseWords.java
@@ -1,8 +1,18 @@
 package com.codeWars.reversesWords;
 
+import java.util.Arrays;
+
 public class ReverseWords {
-    public String reversWords(String words)
-    {
-        return words;
+    public String reversWords(String words) {
+
+        char[] reversWord = new char[words.length()];
+        int count = 0;
+
+        for (int index = words.length() - 1; index >= 0; index--) {
+            reversWord[count] = words.charAt(index);
+            count++;
+        }
+       return String.valueOf(reversWord);
+
     }
 }
