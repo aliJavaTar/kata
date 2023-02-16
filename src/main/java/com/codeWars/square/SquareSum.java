aa6721a95de354198babea5de084b27427186a4a
aliJavaTar <ali77erfagh99@gmail.com>
2023-02-16 20:10:25 +0330

    pass test

--- src/main/java/com/codeWars/square/SquareSum.java
+++ src/main/java/com/codeWars/square/SquareSum.java
@@ -1,7 +1,15 @@
 package com.codeWars.square;
 
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+import static java.lang.Math.*;
+
 public class SquareSum {
-    public int sum(int[] n) {
-        return 0;
+    public int sum(int[] numbers) {
+        return Arrays.stream(numbers)
+                .map(number -> (int) pow(number, 2))
+                .sum();
     }
 }
--- src/test/java/com/codeWars/square/SquareSumTest.java
+++ src/test/java/com/codeWars/square/SquareSumTest.java
@@ -3,7 +3,7 @@ package com.codeWars.square;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class SquareSumTest {
     private SquareSum squareSum;
@@ -17,8 +17,26 @@ class SquareSumTest {
     void should_give_one_when_pass_a_one() {
         int[] array = new int[1];
         array[0] = 1;
+        int sum = squareSum.sum(array);
+        assertThat(sum).isEqualTo(1);
 
-        squareSum.sum(array);
+        array[0] = 2;
+        int sum1 = squareSum.sum(array);
+        assertThat(sum1).isEqualTo(4);
+
+        array[0] = 5;
+        int sum2 = squareSum.sum(array);
+        assertThat(sum2).isEqualTo(25);
+
+    }
+
+    @Test
+    void should_give_number_of_array_and_pow_and_sum_numbers() {
+        int[] array = new int[2];
+        array[0] = 1;
+        array[1] = 2;
+        int sum = squareSum.sum(array);
+        assertThat(sum).isEqualTo(5);
     }
 
 }
