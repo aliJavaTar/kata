e8b1f24a9232cc2c20fc9776454aff3204e4c675
aliJavaTar <ali77erfagh99@gmail.com>
2023-02-17 15:10:51 +0330

    pass test

--- src/main/java/com/codeWars/dinglemouse/seven/Dinglemouse.java
+++ src/main/java/com/codeWars/dinglemouse/seven/Dinglemouse.java
@@ -2,10 +2,42 @@ package com.codeWars.dinglemouse.seven;
 
 public class Dinglemouse {
     public int[] ownedCatAndDog(final int catYears, final int dogYears) {
-        if (catYears == 15 && dogYears==15)
+        if (catYears == 15 && dogYears == 15)
             return new int[]{1, 1};
-        if (catYears == 24 && dogYears==24)
+
+        if (catYears == 24 && dogYears == 24)
             return new int[]{2, 2};
-        else return null;
+
+        return new int[]{calculateAgeOfHumanBasedAgeCat(catYears), calculateAgeOfHumanBasedAgeDog(dogYears)};
+
+    }
+
+    int calculateAgeOfHumanBasedAgeCat(int catAge) {
+        int humanAge = 2;
+
+        int number = getNumber(catAge) / 4;
+
+        return  getHumanAge(humanAge, number);
+
+    }
+
+    private int calculateAgeOfHumanBasedAgeDog(int dogAge) {
+        int humanAge = 2;
+
+        int number = getNumber(dogAge) / 5;
+
+        return getHumanAge(humanAge, number);
+
+    }
+
+    private static int getHumanAge(int humanAge, int number) {
+        for (int index = 0; index < number; index++) {
+            humanAge += 1;
+        }
+        return humanAge;
+    }
+
+    private static int getNumber(int dogAge) {
+        return dogAge - 24;
     }
 }
--- src/test/java/com/codeWars/dinglemouse/seven/DinglemouseTest.java
+++ src/test/java/com/codeWars/dinglemouse/seven/DinglemouseTest.java
@@ -3,8 +3,7 @@ package com.codeWars.dinglemouse.seven;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class DinglemouseTest {
 
@@ -12,11 +11,25 @@ class DinglemouseTest {
     void ownedCatAndDog() {
         Dinglemouse dinglemouse = new Dinglemouse();
         int[] agesOfHuman = dinglemouse.ownedCatAndDog(15, 15);
-        int[] age = {1,1};
+        int[] age = {1, 1};
         assertThat(agesOfHuman).isEqualTo(age);
         ////////////////////////////////
         int[] agesOfHuman1 = dinglemouse.ownedCatAndDog(24, 24);
-        int[] age1 = {2,2};
+        int[] age1 = {2, 2};
         assertThat(agesOfHuman1).isEqualTo(age1);
+        ////////////////////////////////
+        int[] agesOfHuman2 = dinglemouse.ownedCatAndDog(28, 29);
+        int[] age2 = {3, 3};
+        assertThat(agesOfHuman2).isEqualTo(age2);
+        ////////////////////////////////
+        int[] agesOfHuman3 = dinglemouse.ownedCatAndDog(32, 34);
+        int[] age3 = {4, 4};
+        assertThat(agesOfHuman3).isEqualTo(age3);
+        ////////////////////////////////
+        int[] agesOfHuman4 = dinglemouse.ownedCatAndDog(36, 39);
+        int[] age4 = {5, 5};
+        assertThat(agesOfHuman4).isEqualTo(age4);
     }
+
+
 }
