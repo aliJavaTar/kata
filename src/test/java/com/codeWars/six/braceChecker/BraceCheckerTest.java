f2756f628cc0434453af36a0f2c2b2b9414c2817
A.erfagh <ali77erfagh99@gmail.com>
2023-02-25 13:20:15 +0330

    two test pass

--- src/test/java/com/codeWars/six/braceChecker/BraceCheckerTest.java
+++ src/test/java/com/codeWars/six/braceChecker/BraceCheckerTest.java
@@ -1,16 +1,42 @@
 package com.codeWars.six.braceChecker;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 class BraceCheckerTest {
-    @Test
-    void should_give_true_when_order_of_the_braces_is_valid() {
+    private BraceChecker brace;
 
-        BraceChecker brace = new BraceChecker();
-        boolean valid = brace.isValid("(");
-        Assertions.assertFalse(valid);
+    @BeforeEach
+    void setUp() {
+        brace = new BraceChecker();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"(", ")", "}", "{", "[", "]"})
+    void should_give_false_when_order_of_the_braces_is_not_valid_Whit_one_character(String value) {
+
+        assertFalse(brace.isValid(value));
+
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"()", "{}", "[]"})
+    void should_give_true_when_order_of_the_braces_is_not_valid_whit_two_character(String value) {
+
+        assertTrue(brace.isValid(value));
+
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {")(", "}{", "]["})
+    void should_give_false_when_order_of_the_braces_is_not_valid_Whit_many_character(String value) {
+
+        assertFalse(brace.isValid(value));
 
     }
 }
