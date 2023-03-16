a76a73afb014c8cf5ffd151563de92e885bcf46e
aliJavaTar <ali77erfagh99@gmail.com>
2023-03-16 17:50:05 +0330

    pass first test

--- src/main/java/com/codeWars/RepeatIt.java
+++ src/main/java/com/codeWars/RepeatIt.java
@@ -2,6 +2,9 @@ package com.codeWars;
 
 public class RepeatIt {
     public String repeatString(final Object toRepeat, final int number) {
+        if (!(toRepeat instanceof String))
+            return "Not a string";
+
         return "";
     }
 }
--- src/test/java/com/codeWars/RepeatItTest.java
+++ src/test/java/com/codeWars/RepeatItTest.java
@@ -1,13 +1,26 @@
 package com.codeWars;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RepeatItTest {
-     private RepeatIt repeat ;
     @Test
-    void repeatString() {
-        repeat =new RepeatIt();
+    void shouldGiveMessage_Not_a_string_WhenToRepeatIsNotString() {
+        RepeatIt repeat = new RepeatIt();
+
+        String message = repeat.repeatString(23, 11);
+
+        Assertions.assertThat(message).isEqualTo("Not a string");
+    }
+
+    @Test
+    void shouldGiveWhenToRepeatIsNotString() {
+        RepeatIt repeat = new RepeatIt();
+
+        String message = repeat.repeatString(23, 11);
+
+        Assertions.assertThat(message).isEqualTo("Not a string");
     }
 }
