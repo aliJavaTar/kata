f13a410c59dfc63eb1e1d63ab3c24b0270be3916
aliJavaTar <ali77erfagh99@gmail.com>
2023-04-11 22:08:30 +0330

    test pass

--- src/main/java/com/codeWars/CalculatePowOfArray.java
+++ src/main/java/com/codeWars/CalculatePowOfArray.java
@@ -1,4 +1,18 @@
 package com.codeWars;
 
 public class CalculatePowOfArray {
+    public int calculate(int[] array, int number) {
+        // { 1 , 2 , 3 , 4 }
+        if (array.length >= number) {
+            int num = array[number];
+            int pow = 1;
+
+            for (int index = 1; index <= number; index++) {
+                pow = num * pow;
+            }
+            return pow;
+        } return -1;
+
+
+    }
 }
--- src/test/java/com/codeWars/CalculatePowOfArrayTest.java
+++ src/test/java/com/codeWars/CalculatePowOfArrayTest.java
@@ -1,5 +1,6 @@
 package com.codeWars;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,8 +8,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalculatePowOfArrayTest {
 
     @Test
-    void should_give_number_of_index_array_and_calculate_pow_of_this_number()
-    {
-
+    void should_give_number_of_index_array_and_calculate_pow_of_this_number() {
+        CalculatePowOfArray calculate = new CalculatePowOfArray();
+        int[] array = {1, 2, 3, 4};
+        int actual = calculate.calculate(array, 2);
+        Assertions.assertThat(actual).isEqualTo(9);
     }
 }
