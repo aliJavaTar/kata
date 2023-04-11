e8cb45d3ffaecde5223d04a3a7bb2bd3dadea978
aliJavaTar <ali77erfagh99@gmail.com>
2023-04-11 21:40:17 +0330

    new kata add

---
+++ src/test/java/com/codeWars/CalculatePowOfArrayTest.java
@@ -0,0 +1,14 @@
+package com.codeWars;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatePowOfArrayTest {
+
+    @Test
+    void should_give_number_of_index_array_and_calculate_pow_of_this_number()
+    {
+
+    }
+}
