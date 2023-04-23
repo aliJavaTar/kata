6eeb5870fa408c006ca594acac5139a31f5f7be5
aliJavaTar <ali77erfagh99@gmail.com>
2023-04-23 15:13:06 +0330

    add first test for even number

--- src/test/java/com/codeWars/six/find_out_lier/FindOutlierTest.java
+++ src/test/java/com/codeWars/six/find_out_lier/FindOutlierTest.java
@@ -3,7 +3,7 @@ package com.codeWars.six.find_out_lier;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class FindOutlierTest {
 
@@ -15,12 +15,14 @@ class FindOutlierTest {
     }
 
     @Test
-    void should_find_odd_number() {
+    void should_find_even_number() {
+        int[] numbers = {11111, 44343, 45645643, 442345243, 3, 5, 2};
 
+        assertThat(findOutlier.find(numbers)).isEqualTo(2);
     }
 
     @Test
-    void should_find_even_number() {
+    void should_find_odd_number() {
 
     }
 }
