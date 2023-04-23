dd11c7f3c89aeaf97617e6499c6e1a7721407be2
aliJavaTar <ali77erfagh99@gmail.com>
2023-04-23 14:58:37 +0330

    add test for TDD approach

--- src/main/java/com/codeWars/six/find_out_lier/FindOutlier.java
+++ src/main/java/com/codeWars/six/find_out_lier/FindOutlier.java
@@ -1,4 +1,7 @@
 package com.codeWars.six.find_out_lier;
 
 public class FindOutlier {
+    public int find(int[] integers) {
+        return 0;
+    }
 }
---
+++ src/test/java/com/codeWars/six/find_out_lier/FindOutlierTest.java
@@ -0,0 +1,26 @@
+package com.codeWars.six.find_out_lier;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FindOutlierTest {
+
+    private FindOutlier findOutlier;
+
+    @BeforeEach
+    void setUp() {
+        findOutlier = new FindOutlier();
+    }
+
+    @Test
+    void should_find_odd_number() {
+
+    }
+
+    @Test
+    void should_find_even_number() {
+
+    }
+}
