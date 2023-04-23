7c1fb61f64cbbca24434b9bd6257215c4a4882ad
aliJavaTar <ali77erfagh99@gmail.com>
2023-04-23 15:38:20 +0330

    all test pass

--- src/test/java/com/codeWars/six/find_out_lier/FindOutlierTest.java
+++ src/test/java/com/codeWars/six/find_out_lier/FindOutlierTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FindOutlierTest {
 
@@ -31,17 +32,15 @@ class FindOutlierTest {
 
     @Test
     void should_find_odd_number() {
-        int[] numbers = {3, 4, 2};
+        int[] numbers = {2,6,8,-10,3};
 
         assertThat(findOutlier.find(numbers)).isEqualTo(3);
 
-//        int[] numbers_1 = {11, 22, 35};
-//
-//        assertThat(findOutlier.find(numbers_1)).isEqualTo(22);
-//
-//        int[] numbers_2 = {1235, 213432, 355555555};
-//
-//        assertThat(findOutlier.find(numbers_2)).isEqualTo(213432);
+
+        int[] exampleTest2 = {206847684,1056521,7,17,1901,21104421,7,1,35521,1,7781};
+        int[] exampleTest3 = {Integer.MAX_VALUE, 0, 1};
+        assertEquals(206847684, findOutlier.find(exampleTest2));
+        assertEquals(0, findOutlier.find(exampleTest3));
     }
 
 }
