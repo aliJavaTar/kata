296a58938bf723ded495a6960b632cb2a64722f6
aliJavaTar <ali77erfagh99@gmail.com>
2023-04-23 15:34:39 +0330

    all test pass

--- src/main/java/com/codeWars/six/find_out_lier/FindOutlier.java
+++ src/main/java/com/codeWars/six/find_out_lier/FindOutlier.java
@@ -2,7 +2,32 @@ package com.codeWars.six.find_out_lier;
 
 public class FindOutlier {
     public int find(int[] integers) {
+        int countOfEven = 0;
+        int countOfOdd = 0;
 
+        for (int index = 0; index < 3; index++) {
+            if (isEven(integers[index])) {
+                countOfEven++;
+            }
+            if (!isEven(integers[index])) {
+                countOfOdd++;
+            }
+        }
+        if (countOfEven > countOfOdd) {
+            for (int integer : integers) {
+                if (!isEven(integer))
+                    return integer;
+            }
+        } else {
+            for (int integer : integers) {
+                if (isEven(integer))
+                    return integer;
+            }
+        }
         return 0;
+    }
+
+    private boolean isEven(int number) {
+        return number % 2 == 0;
     }
 }
--- src/test/java/com/codeWars/six/find_out_lier/FindOutlierTest.java
+++ src/test/java/com/codeWars/six/find_out_lier/FindOutlierTest.java
@@ -16,13 +16,32 @@ class FindOutlierTest {
 
     @Test
     void should_find_even_number() {
-        int[] numbers = {11111, 44343, 45645643, 442345243, 3, 5, 2};
+        int[] numbers = {3, 5, 2};
 
         assertThat(findOutlier.find(numbers)).isEqualTo(2);
+
+        int[] numbers_1 = {11, 22, 35};
+
+        assertThat(findOutlier.find(numbers_1)).isEqualTo(22);
+
+        int[] numbers_2 = {1235, 213432, 355555555};
+
+        assertThat(findOutlier.find(numbers_2)).isEqualTo(213432);
     }
 
     @Test
     void should_find_odd_number() {
+        int[] numbers = {3, 4, 2};
 
+        assertThat(findOutlier.find(numbers)).isEqualTo(3);
+
+//        int[] numbers_1 = {11, 22, 35};
+//
+//        assertThat(findOutlier.find(numbers_1)).isEqualTo(22);
+//
+//        int[] numbers_2 = {1235, 213432, 355555555};
+//
+//        assertThat(findOutlier.find(numbers_2)).isEqualTo(213432);
     }
+
 }
