ccdf1c7a4d90cf54596799f373894784c3a1b4d1
ali <ali77erfagh99@gmail.com>
2024-08-04 13:16:39 +0330

    change structure

--- src/main/java/com/leetcode/leetcodeCreateTargetArray/TargetArray.java
+++
@@ -1,8 +0,0 @@
-package com.leetcode.leetcodeCreateTargetArray;
-
-public class TargetArray {
-    public int[] createTargetArray(int[] numbers, int[] index) {
-        int[] arrayTarget = new int[numbers.length];
-        return null;
-    }
-}
---
+++ src/main/java/com/old/kata/Condition.java
@@ -0,0 +1,29 @@
+package com.old.kata;
+
+public class Condition {
+
+    public int findMax(int firstNumber, int secondNumber, int thirdNumber) {
+
+//       return Math.max(Math.max(firstNumber, secondNumber), thirdNumber);
+
+
+//        int max = firstNumber;
+//        if (secondNumber > max) max = secondNumber;
+//        if (thirdNumber > max) max = thirdNumber;
+//        return max;
+
+
+//        if (firstNumber > secondNumber && firstNumber > thirdNumber)
+//            return firstNumber;
+//
+//        else if (secondNumber > firstNumber && secondNumber > thirdNumber)
+//            return secondNumber;
+//
+//        else if (thirdNumber > firstNumber && thirdNumber > secondNumber)
+//            return thirdNumber;
+//
+//        return 0;
+ return  0;
+    }
+
+}
