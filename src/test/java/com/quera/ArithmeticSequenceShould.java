5ae1795c85b3d080255a03e5bb103a4bdf49d69a
ali <ali77erfagh99@gmail.com>
2024-08-10 08:59:52 +0330

    add new kata

---
+++ src/test/java/com/quera/ArithmeticSequenceShould.java
@@ -0,0 +1,13 @@
+package com.quera;
+
+import org.junit.jupiter.api.Test;
+
+
+
+class ArithmeticSequenceShould {
+
+    @Test
+    public void calculate_minimum_of_amount_changes_from_sequence() {
+
+    }
+}
