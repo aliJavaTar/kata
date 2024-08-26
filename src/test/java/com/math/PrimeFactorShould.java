3d7fe51629cf842a9f76e79209efaaa4e624cd59
ali <ali77erfagh99@gmail.com>
2024-08-26 11:19:28 +0330

    i dont know

--- src/test/java/com/math/PrimeFactorShould.java
+++ src/test/java/com/math/PrimeFactorShould.java
@@ -1,24 +1,26 @@
-package com.math;
-
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-class PrimeFactorShould {
-
-    @ParameterizedTest
-    @CsvSource({
-            "12, 2",
-            "15, 2",
-            "315, 3",
-            "1092, 4"
-    })
-    void testCalculate(int input, int expectedCount) {
-        var primeNumber = new PrimeNumber();
-        var primeFactor = new PrimeFactor(primeNumber);
-
-        int actualCount = primeFactor.calculate(input);
-        assertEquals(expectedCount, actualCount, "The count of prime factors should match");
-    }
-}
+//package com.math;
+//
+//import org.junit.jupiter.params.ParameterizedTest;
+//import org.junit.jupiter.params.provider.CsvSource;
+//import org.junit.jupiter.params.provider.EnumSource;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//
+//class PrimeFactorShould {
+//
+//
+//    @ParameterizedTest
+//    @CsvSource({
+//            "12, 2",
+//            "15, 2",
+//            "315, 3",
+//            "1092, 4"
+//    })
+//    void testCalculate(int input, int expectedCount) {
+//        var primeNumber = new PrimeNumber();
+//        var primeFactor = new PrimeFactor(primeNumber);
+//
+//        int actualCount = primeFactor.calculate(input);
+//        assertEquals(expectedCount, actualCount, "The count of prime factors should match");
+//    }
+//}
