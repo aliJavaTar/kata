c852f881ed345717d1b9c0fe422afa455a02d709
ali <ali77erfagh99@gmail.com>
2024-08-27 13:58:36 +0330

    refactor test

--- src/test/java/com/math/Exactly3DivisorsTest.java
+++ src/test/java/com/math/Exactly3DivisorsTest.java
@@ -9,7 +9,5 @@ class Exactly3DivisorsTest {
         Exactly3Divisors e = new Exactly3Divisors();
         int result = e.exactly3Divisors(10);
         Assertions.assertThat(result).isEqualTo(2);
-        System.out.println(result);
-//        Arrays.fill();
     }
 }
