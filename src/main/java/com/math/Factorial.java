66a06703ad2efdb1e4586022239e49ce74105d1a
ali <ali77erfagh99@gmail.com>
2024-08-27 09:09:45 +0330

    i dont know

--- src/main/java/com/math/Factorial.java
+++ src/main/java/com/math/Factorial.java
@@ -1,11 +1,13 @@
 package com.math;
 
+import java.math.BigDecimal;
+
 public record Factorial(int number) {
 
-    public int calculateFactorialOfNumber() {
-        int result = 1;
+    public BigDecimal calculateFactorialOfNumber() {
+        BigDecimal result = new BigDecimal(1);
         for (int index = number; index > 0; index--) {
-            result *= index;
+            result = result.multiply(BigDecimal.valueOf(index));
         }
         return result;
     }
