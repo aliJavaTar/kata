7e575a65a7b782cdf33e09ce03e89a4cd4f52a12
ali <ali77erfagh99@gmail.com>
2024-09-01 14:12:05 +0330

    refactor code

--- src/test/java/com/codeforces/AddStringTest.java
+++ src/test/java/com/codeforces/AddStringTest.java
@@ -1,6 +1,6 @@
 package com.codeforces;
 
-import static org.junit.jupiter.api.Assertions.*;
+
 
 import com.old.leetcode.easy.AddStrings;
 import org.assertj.core.api.Assertions;
@@ -9,19 +9,14 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 class AddStringTest {
 
-    public static final String FORMAT = "Addition of %s and %s should be %s";
 
     @ParameterizedTest
     @CsvSource({
             "0,0,0",
             "12, 12, 24",
-//            "-123, 456, 333",
-//            "123, -456, -333",
-//            "-123, -456, -579",
             "0, 0, 0",
             "0, 123, 123",
             "123, 0, 123",
-//            "-123, 0, -123"
     })
     void testAddition(String firstNumber, String secondNumber, String expected) {
         var addStrings = new AddStrings(firstNumber, secondNumber);
