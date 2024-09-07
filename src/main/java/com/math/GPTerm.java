edb037176712ad6f3b1a17dba782e53cb1359dbc
ali <ali77erfagh99@gmail.com>
2024-09-07 15:01:18 +0330

    add new algorithm

--- src/main/java/com/math/GPTerm.java
+++ src/main/java/com/math/GPTerm.java
@@ -2,15 +2,11 @@ package com.math;
 
 public class GPTerm {
     public int GpTerm(int a, int b, int n) {
-        {
-            if (n == 1)
-                return a;
-            int result = a;
 
-            for (int index = a; index <= n; index += b) {
-                result *= index;
-            }
-            return result;
-        }
+        if (n == 1)
+            return a;
+        double r = (double) b / a;
+        double pow = Math.pow(r, n - 1);
+        return (int) Math.round(a * pow);
     }
 }
--- src/test/java/com/math/GPTermTest.java
+++ src/test/java/com/math/GPTermTest.java
@@ -1,7 +1,16 @@
 package com.math;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class GPTermTest {
+    @Test
+    void test() {
+        GPTerm gpTerm = new GPTerm();
+        int result = gpTerm.GpTerm(1, 2, 5);
+        Assertions.assertThat(result).isEqualTo(16);
 
+         result = gpTerm.GpTerm(84, 87, 3);
+        Assertions.assertThat(result).isEqualTo(90);
+    }
 }
