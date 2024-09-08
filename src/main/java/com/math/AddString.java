96fc23f8897e28fd05d601e57b50a084047457fb
ali <ali77erfagh99@gmail.com>
2024-09-08 12:00:41 +0330

    add new algorithm

--- src/main/java/com/math/AddString.java
+++ src/main/java/com/math/AddString.java
@@ -2,35 +2,41 @@ package com.math;
 
 public class AddString {
     public String addStrings(String firstNumber, String secondNumber) {
-        int lastIndex = firstNumber.length() - 1;
-        int lastIndexSecond = secondNumber.length() - 1;
+//        int lastIndex = firstNumber.length() - 1;
+//        int lastIndexSecond = secondNumber.length() - 1;
+//        int carry = 0;
+//        StringBuilder builder = new StringBuilder();
+//
+//        while (lastIndex >= 0 || lastIndexSecond >= 0 || carry == 1) {
+//            if (lastIndex >= 0) {
+//                carry += firstNumber.charAt(lastIndex--) - '0';
+//                lastIndex--;
+//            }
+//            if (lastIndexSecond >= 0) {
+//                carry += secondNumber.charAt(lastIndexSecond--) - '0';
+//                lastIndexSecond--;
+//            }
+//            builder.insert(0, carry % 10);
+//            carry /= 10;
+//        }
+//        return builder.toString();
+        int lastIndexOfFirstNumber = firstNumber.length() - 1;
+        int lastIndexOfSecondNumber = secondNumber.length() - 1;
         int carry = 0;
-        StringBuilder stringBuilder = new StringBuilder();
-
-        while (lastIndex >= 0 || lastIndexSecond >= 0 || carry == 1) {
-            if (lastIndex >= 0) {
-                carry += firstNumber.charAt(lastIndex--) - '0';
-                lastIndex--;
+        StringBuilder builder = new StringBuilder();
+        while (lastIndexOfFirstNumber >= 0 || lastIndexOfSecondNumber >= 0 || carry == 1) {
+            if (lastIndexOfFirstNumber >= 0) {
+                carry += firstNumber.charAt(lastIndexOfFirstNumber) - '0';
+                lastIndexOfFirstNumber--;
             }
-            if (lastIndexSecond >= 0) {
-                carry += secondNumber.charAt(lastIndexSecond--) - '0';
-                lastIndexSecond--;
+            if (lastIndexOfSecondNumber >= 0) {
+                carry += secondNumber.charAt(lastIndexOfSecondNumber) - '0';
+                lastIndexOfSecondNumber--;
             }
-            stringBuilder.insert(0, carry % 10);
+            builder.insert(0, carry % 10);
             carry /= 10;
         }
-        return stringBuilder.toString();
-        // Alternative solution using Java 8 streams
-        // return Stream.of(firstNumber, secondNumber)
-        //        .mapToInt(String::chars)
-        //        .map(digit -> digit - '0')
-        //        .reduce(0, (a, b) -> (a + b) % 10 + (a + b) / 10);
 
-        // Alternative solution using Java 8 streams and StringBuilder
-        // return Stream.of(firstNumber, secondNumber)
-        //        .mapToInt(String::chars)
-        //        .map(digit -> digit - '0')
-        //        .reduce(new StringBuilder(), (builder, digit) -> builder.append((digit + carry) % 10), StringBuilder::append);
-
+        return builder.toString();
     }
 }
