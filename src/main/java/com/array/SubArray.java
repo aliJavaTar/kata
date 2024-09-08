b63f983cebff8e4d9d682147d5b555973ca4a277
ali <ali77erfagh99@gmail.com>
2024-09-08 15:46:40 +0330

    add new solution Kadane's Algorithm

--- src/main/java/com/array/SubArray.java
+++ src/main/java/com/array/SubArray.java
@@ -16,17 +16,14 @@ public record SubArray(int[] array) {
     }
 
     public static int calculateMax(int[] array) {
-        int maximumSum = array[0];
+        int maxSum = array[0];
+        int curSum = 0;
 
-        for (int index = 0; index < array.length; index++) {
-            int currentSum = 0;
-            for (int j = index + 1; j < array.length; j++) {
-                currentSum +=  array[j];
-                if (currentSum > maximumSum) {
-                    maximumSum = currentSum;
-                }
-            }
+        for (int n : array) {
+            curSum = Math.max(curSum, 0);
+            curSum += n;
+            maxSum = Math.max(maxSum, curSum);
         }
-        return maximumSum;
+        return maxSum;
     }
 }
