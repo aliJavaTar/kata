030140582ad4f3eab0496e2fccda8e4429338ee3
ali <ali77erfagh99@gmail.com>
2024-09-11 12:26:45 +0330

    add first logic

--- src/main/java/com/array/Find.java
+++ src/main/java/com/array/Find.java
@@ -1,7 +1,13 @@
 package com.array;
 
+import java.util.Arrays;
+
 public record Find(double[] array) {
     double findTheUniqueNumber() {
+        Arrays.sort(array);
 
+        if (array[0] != array[1] && array[0] != array[array.length - 1])
+            return array[0];
+        return array[array.length - 1];
     }
 }
--- src/test/java/com/array/FindTest.java
+++ src/test/java/com/array/FindTest.java
@@ -4,7 +4,12 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class FindShould{
- @Test
-    
+class FindShould {
+    @Test
+    void find_unique_number_in_list() {
+        double[] array = {2, 2, 3, 2, 2};
+        var find = new Find(array);
+        double uniqueNumber = find.findTheUniqueNumber();
+        assertEquals(3, uniqueNumber);
+    }
 }
