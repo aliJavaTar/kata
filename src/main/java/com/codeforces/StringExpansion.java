0de48f3cf8b78ae9fc23ccfbc8aa1e2f26a7f312
aliJavaTar <ali77erfagh99@gmail.com>
2024-09-20 14:11:18 +0330

    add new test

--- src/main/java/com/codeforces/StringExpansion.java
+++ src/main/java/com/codeforces/StringExpansion.java
@@ -6,20 +6,40 @@ import static java.lang.Integer.parseInt;
 
 public class StringExpansion {
     private final String letters;
+    private int indexOfFirstParenthesis = -1;
 
     public StringExpansion(String letters) {
         this.letters = letters;
     }
 
     public String expandedLetter() {
-        StringBuilder result = new StringBuilder();
-        int multiplier;
-        Stack<Character> stack = new Stack<>();
+
+        Stack<Character> stackAfterParenthesis = new Stack<>();
+        Stack<Character> stackBeforeParenthesis = new Stack<>();
         for (int index = 0; index < letters.length() - 1; index++) {
-            if (letters.charAt(index) != ')' && letters.charAt(index) != '(') {
-                stack.push(letters.charAt(index));
+            if (letters.charAt(index) == '(' && indexOfFirstParenthesis == -1) {
+                indexOfFirstParenthesis = index;
+                if (index > 0 && Character.isDigit(letters.charAt(index - 1))) {
+                    stackAfterParenthesis.push(letters.charAt(index - 1));
+                }
+            }
+            if (letters.charAt(index) != ')' && letters.charAt(index) != '(' && indexOfFirstParenthesis == -1) {
+                stackBeforeParenthesis.push(letters.charAt(index));
+            }
+            if (letters.charAt(index) != ')' && letters.charAt(index) != '(' && indexOfFirstParenthesis != -1) {
+                stackAfterParenthesis.push(letters.charAt(index));
             }
         }
+
+        var resultAfterPar = calculateResult(stackAfterParenthesis);
+        var resultBeforePar = calculateResult(stackBeforeParenthesis);
+        StringBuilder result = resultAfterPar.append(resultBeforePar);
+        return result.reverse().toString();
+    }
+
+    private StringBuilder calculateResult(Stack<Character> stack) {
+        var result = new StringBuilder();
+        int multiplier;
         String temp = "";
         while (stack.iterator().hasNext()) {
             Character character = stack.pop();
@@ -28,9 +48,10 @@ public class StringExpansion {
                 result.append(temp.repeat(Math.max(0, multiplier - 1)));
             } else
                 temp = result.append(character).toString();
-        }
 
-        return result.reverse().toString();
+
+        }
+        return result;
     }
 
 
--- src/test/java/com/codeforces/StringExpansionMust.java
+++ src/test/java/com/codeforces/StringExpansionMust.java
@@ -15,10 +15,22 @@ class StringExpansionMust {
         result = expansion.expandedLetter();
         expected = "acc";
         Assertions.assertThat(result).isEqualTo(expected);
-         expansion = new StringExpansion("k(a3(b(a2(c))))");
-         result = expansion.expandedLetter();
-         expected = "kabaccbaccbacc";
+
+        expansion = new StringExpansion("k2(a3(b(a2(c))))");
+        result = expansion.expandedLetter();
+        expected = "kabaccbaccbacc";
         Assertions.assertThat(result).isEqualTo(expected);
+
+         expansion = new StringExpansion("2y4(cd)");
+         result = expansion.expandedLetter();
+         expected = "yycdcdcdcd";
+        Assertions.assertThat(result).isEqualTo(expected);
+
+//        expansion = new StringExpansion("3(ab)4(cd)");
+//        result = expansion.expandedLetter();
+//        expected = "abababcdcdcdcd";
+//        Assertions.assertThat(result).isEqualTo(expected);
+
 
     }
 
