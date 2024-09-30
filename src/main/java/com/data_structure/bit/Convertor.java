86bd1f25c5ab99b07f5e09a0e34299bde1b2e467
ali <ali77erfagh99@gmail.com>
2024-09-30 15:25:05 +0330

    add new logic

--- src/main/java/com/data_structure/bit/Convertor.java
+++ src/main/java/com/data_structure/bit/Convertor.java
@@ -1,20 +1,47 @@
 package com.data_structure.bit;
 
-public record Convertor(int number) {
+public class Convertor {
+    private int number;
+
+    public Convertor(int number) {
+        this.number = number;
+    }
 
     String convertNumberToBinary() {
-        var index = getCountOfNumber();
-        System.out.println(index);
-        return "";
+        var countOfNumber = getCountOfNumber();
+        var stringBuilder = new StringBuilder();
+        int pow = 0;
+        while (countOfNumber != -1) {
+            if (number - (int) Math.pow(2, pow) >= 0)
+                stringBuilder.append(1);
+            else
+                stringBuilder.append(0);
+
+            number = number - (int) Math.pow(2, countOfNumber);
+            countOfNumber--;
+            pow++;
+        }
+
+        return stringBuilder.toString();
     }
 
     private int getCountOfNumber() {
+        int countOfNumber = 0;
         for (int index = 0; index < number; index++) {
-            int pow = (int) Math.pow(2, index);
-            if (pow >= number)
-                return index;
+            if ((int) Math.pow(2, index) >= number)
+                countOfNumber++;
         }
-        return number;
+        return countOfNumber;
     }
 
 }
+//        while (countOfNumber != 0) {
+//            if (number - (int) Math.pow(2, countOfNumber) == 0)
+//                stringBuilder.append(1);
+//
+//            number = (number - (int) Math.pow(2, countOfNumber));
+//            countOfNumber--;
+//
+//            if (number == 0)
+//                stringBuilder.append(0);
+//        }
--- src/test/java/com/data_structure/bit/ConvertorShould.java
+++ src/test/java/com/data_structure/bit/ConvertorShould.java
@@ -9,6 +9,19 @@ class ConvertorShould {
     void convertNumberToBinary() {
         var convertor = new Convertor(2);
         String result = convertor.convertNumberToBinary();
-        assertEquals("01", result);
+        assertEquals("10", result);
+
+        convertor = new Convertor(3);
+        result = convertor.convertNumberToBinary();
+        assertEquals("11", result);
+
+        convertor = new Convertor(4);
+        result = convertor.convertNumberToBinary();
+        assertEquals("100", result);
+
+        convertor = new Convertor(6);
+        result = convertor.convertNumberToBinary();
+        assertEquals("100", result);
+
     }
 }
