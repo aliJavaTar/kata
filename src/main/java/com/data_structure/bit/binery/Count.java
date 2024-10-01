0d7513518c150053e5e59a674cbbbecfa5097644
ali <ali77erfagh99@gmail.com>
2024-10-01 11:13:10 +0330

    pass all test

--- src/main/java/com/data_structure/bit/binery/Count.java
+++ src/main/java/com/data_structure/bit/binery/Count.java
@@ -1,4 +1,23 @@
 package com.data_structure.bit.binery;
 
-public class Count {
+
+
+public record Count(int number) {
+
+    public int calculate() {
+        int count = 0;
+        int temp = number;
+        int index = (number / 2);
+        while (temp >= 0 && index >= 0) {
+            if (temp - Math.pow(2, index) >= 0) {
+                count++;
+                temp = temp - (int) Math.pow(2, index);
+            }
+
+            index--;
+
+        }
+        return count;
+    }
+
 }
--- src/test/java/com/data_structure/bit/binery/CountShould.java
+++ src/test/java/com/data_structure/bit/binery/CountShould.java
@@ -1,12 +1,27 @@
 package com.data_structure.bit.binery;
 
-import org.junit.jupiter.api.Test;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class CountShould {
-    @Test
-    void calculateCountOfSubtractionsOfPowersOfTwoUntilNegative  () {
-
+    @ParameterizedTest
+    @CsvSource({
+            "2, 1",
+            "4, 1",
+            "5, 2",
+            "6, 2",
+            "7, 3",
+            "8, 1",
+            "9, 2",
+            "10, 2",
+            "11, 3",
+            "12, 2",
+    })
+    void calculateCountOfSubtractionsOfPowersOfTwoUntilNegative(int input, int expected) {
+        int calculate = new Count(input).calculate();
+        System.out.println(calculate);
+        Assertions.assertThat(calculate).isEqualTo(expected);
     }
 }
