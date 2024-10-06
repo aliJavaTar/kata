0336f5be2dd40a2a700b30db70629531bc6f1f4b
ali <ali77erfagh99@gmail.com>
2024-10-06 10:59:42 +0330

    add two method for solve problem

--- src/main/java/com/data_structure/bit/Find.java
+++ src/main/java/com/data_structure/bit/Find.java
@@ -3,9 +3,14 @@ package com.data_structure.bit;
 public record Find(int number) {
     public int findMSB() {
         int result = 1;
-        while ((result & number) != number) {
+        while (result < number) {
             result <<= 1;
         }
+
         return result >> 1;
+    }
+
+    public int findMSB_performance() {
+      return Integer.highestOneBit(number);
     }
 }
