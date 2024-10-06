5baac676c9847aaf99239975997b4fc1bd8d01ef
ali <ali77erfagh99@gmail.com>
2024-10-06 10:15:01 +0330

    refactor class pow

--- src/main/java/com/data_structure/bit/Pow.java
+++ src/main/java/com/data_structure/bit/Pow.java
@@ -2,7 +2,7 @@ package com.data_structure.bit;
 
 public record Pow(int number) {
 
-    public static final int ONE = 1;
+    private static final int ONE = 1;
 
     public boolean isPowOfTwo() {
         return (number - ONE & number) == 0;
