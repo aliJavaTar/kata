821bc1a298ce2dd7d61ffbf2b685e9758d21c76c
ali <ali77erfagh99@gmail.com>
2024-10-08 11:34:51 +0330

    add new method

--- src/main/java/com/data_structure/bit/CountSetBits.java
+++ src/main/java/com/data_structure/bit/CountSetBits.java
@@ -2,6 +2,7 @@ package com.data_structure.bit;
 
 
 import java.math.BigInteger;
+import java.util.BitSet;
 
 public record CountSetBits(BigInteger number) {
     public int calculate() {
@@ -16,4 +17,32 @@ public record CountSetBits(BigInteger number) {
         }
         return count;
     }
+
+    public int calculate_() {
+        int count = 0;
+        int current = number.intValue();
+
+        while (current > 0) {
+            count += current & 1;
+            current >>= 1;
+        }
+        return count;
+    }
+
+    public int calculate__() {
+
+         int[] BitsSetTable256 = new int[256];
+
+        int count = 0;
+        int current = number.intValue();
+
+        while (current > 0) {
+            count += current & 1;
+            current >>= 1;
+        }
+        return count;
+    }
 }
+
+
+
