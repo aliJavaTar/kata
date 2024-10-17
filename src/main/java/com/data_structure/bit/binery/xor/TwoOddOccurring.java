a651e69b15f56d0b78a371cd2bfcbeb2615b811e
ali <ali77erfagh99@gmail.com>
2024-10-17 11:29:36 +0330

    add code

--- src/main/java/com/data_structure/bit/binery/xor/TwoOddOccurring.java
+++ src/main/java/com/data_structure/bit/binery/xor/TwoOddOccurring.java
@@ -1,5 +1,25 @@
 package com.data_structure.bit.binery.xor;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class TwoOddOccurring {
 
+    public static int[] findOddOccurrences(int[] inputArray) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        List<Integer> numbers = new java.util.ArrayList<>(List.of());
+        int index = 0;
+        for (int num : inputArray) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+        }
+
+        for (int num : inputArray) {
+            if (countMap.get(num) % 2 != 0) {
+                numbers.add(num);
+            }
+        }
+        numbers.forEach(System.out::println);
+        return null;
+    }
 }
---
+++ src/test/java/com/data_structure/bit/binery/xor/TwoOddOccurringShould.java
@@ -0,0 +1,27 @@
+package com.data_structure.bit.binery.xor;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TwoOddOccurringShould {
+    @ParameterizedTest
+    @MethodSource("provideTestCases")
+    public void testOddOccurrences(int[] inputArray, int[] expected) {
+        assertArrayEquals(expected, TwoOddOccurring.findOddOccurrences(inputArray));
+    }
+
+    private static Stream<Arguments> provideTestCases() {
+        return Stream.of(
+                Arguments.of(new int[]{12, 23, 34, 12, 12, 23, 12, 45}, new int[]{34, 45}),
+                Arguments.of(new int[]{4, 4, 100, 5000, 4, 4, 4, 4, 100, 100}, new int[]{100, 5000}),
+                Arguments.of(new int[]{10, 20}, new int[]{10, 20}),
+                Arguments.of(new int[]{3, 4, 3, 4, 5, 4, 4, 6, 7, 7}, new int[]{5, 6})
+        );
+    }
+
+}
