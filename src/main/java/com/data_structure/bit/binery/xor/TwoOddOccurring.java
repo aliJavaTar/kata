ebfd3405310626dec9a33ba14265d53d026f5c8f
ali <ali77erfagh99@gmail.com>
2024-10-17 13:21:17 +0330

    add new kata and solve it

--- src/main/java/com/data_structure/bit/binery/xor/TwoOddOccurring.java
+++ src/main/java/com/data_structure/bit/binery/xor/TwoOddOccurring.java
@@ -1,25 +1,9 @@
 package com.data_structure.bit.binery.xor;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class TwoOddOccurring {
 
-    public static int[] findOddOccurrences(int[] inputArray) {
-        Map<Integer, Integer> countMap = new HashMap<>();
-        List<Integer> numbers = new java.util.ArrayList<>(List.of());
-        int index = 0;
-        for (int num : inputArray) {
-            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
-        }
-
-        for (int num : inputArray) {
-            if (countMap.get(num) % 2 != 0) {
-                numbers.add(num);
-            }
-        }
-        numbers.forEach(System.out::println);
-        return null;
+    public static int countBitFlips(int firstNumber, int soundNumber) {
+        int xorResult = firstNumber ^ soundNumber;
+        return (int) Integer.toBinaryString(xorResult).chars().filter(character -> character == '1').count();
     }
 }
