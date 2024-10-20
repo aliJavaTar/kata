1906095c4d0d66d2b6ec6b2f6abdd0eed296138e
ali <ali77erfagh99@gmail.com>
2024-10-20 15:23:24 +0330

    solve kata

--- src/main/java/com/data_structure/bit/binery/FindKthBitInNthBinaryString.java
+++ src/main/java/com/data_structure/bit/binery/FindKthBitInNthBinaryString.java
@@ -4,10 +4,8 @@ import java.util.stream.IntStream;
 
 public record FindKthBitInNthBinaryString(int number, int key) {
 
-    public int findKthBit() {
-        String binaryString = Integer.toBinaryString(number);
-        StringBuilder binaryText = new StringBuilder(binaryString);
-        StringBuilder reverse = binaryText.reverse();
-        return IntStream.range(0, reverse.length() - 1).filter(value -> value == key).findFirst().orElse(0);
+    public boolean findKthBit() {
+        int mask = 1 << key;
+        return (number & mask) != 0;
     }
 }
