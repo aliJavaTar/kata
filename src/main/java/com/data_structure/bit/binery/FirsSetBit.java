759fd6a1654fe025afd1a4c7e35a80b27fe4322a
ali <ali77erfagh99@gmail.com>
2024-10-21 14:30:10 +0330

    last change

--- src/main/java/com/data_structure/bit/binery/FirsSetBit.java
+++ src/main/java/com/data_structure/bit/binery/FirsSetBit.java
@@ -1,4 +1,17 @@
 package com.data_structure.bit.binery;
 
 public class FirsSetBit {
+    private int number;
+
+    public FirsSetBit(int number) {
+        this.number = number;
+    }
+
+    public int find() {
+        while (number > 0) {
+            number = number >> 1;
+
+        }
+        return number;
+    }
 }
--- src/test/java/com/data_structure/bit/binery/FirsSetBitTest.java
+++ src/test/java/com/data_structure/bit/binery/FirsSetBitTest.java
@@ -1,13 +1,15 @@
 package com.data_structure.bit.binery;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class FirsSetBitTest {
 
-    @Test
-    void test() {
-
+    @ParameterizedTest
+    @CsvSource({" 18,2 ", " 12 ,3 "})
+    void test(int input, int excepted) {
+        var firsSetBit = new FirsSetBit(input);
+        Assertions.assertThat(firsSetBit.find()).isEqualTo(excepted);
     }
 }
