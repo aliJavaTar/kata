40853f64abcd2892f42eaa02ec3147f803e8a548
ali <ali77erfagh99@gmail.com>
2024-10-21 14:35:55 +0330

    pass test

--- src/main/java/com/data_structure/bit/binery/K_thBitIsSetOrNot.java
+++ src/main/java/com/data_structure/bit/binery/K_thBitIsSetOrNot.java
@@ -1,4 +1,13 @@
 package com.data_structure.bit.binery;
 
-public class K_thBitIsSetOrNot {
+public record K_thBitIsSetOrNot(int number, int key) {
+    public boolean isSet() {
+        int mask = 1 << key;
+        return (number & mask) != 0;
+    }
 }
+//static boolean checkKthBit(int n, int k) {
+//
+//    int mask = 1 << k;
+//    return (n & mask) != 0;
+//}
--- src/test/java/com/data_structure/bit/binery/K_thBitIsSetOrNotTest.java
+++ src/test/java/com/data_structure/bit/binery/K_thBitIsSetOrNotTest.java
@@ -1,7 +1,16 @@
 package com.data_structure.bit.binery;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class K_thBitIsSetOrNotTest {
+    @ParameterizedTest
+    @CsvSource({"4 , 0 , false , 4 , 1 , true"})
+    void give_true_when_is_set(int number, int key, boolean expected) {
 
+        var kThBitIsSetOrNot = new K_thBitIsSetOrNot(number, key);
+        boolean result = kThBitIsSetOrNot.isSet();
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
 }
