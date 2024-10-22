d5d3fddb56a955a85a28cc3b6ad565fd82cc6372
ali <ali77erfagh99@gmail.com>
2024-10-22 15:27:11 +0330

    writ first test complete and add method i  need

--- src/main/java/com/data_structure/bit/binery/PowerOfTwo.java
+++ src/main/java/com/data_structure/bit/binery/PowerOfTwo.java
@@ -1,4 +1,13 @@
 package com.data_structure.bit.binery;
 
 public class PowerOfTwo {
+    private final int number;
+
+    public PowerOfTwo(int number) {
+        this.number = number;
+    }
+
+    public boolean isPowerOfTwo() {
+        return false;
+    }
 }
--- src/test/java/com/data_structure/bit/binery/PowerOfTwoShould.java
+++ src/test/java/com/data_structure/bit/binery/PowerOfTwoShould.java
@@ -1,7 +1,17 @@
 package com.data_structure.bit.binery;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class PowerOfTwoShould {
 
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 4, 8, 16, 32, 1024})
+    void check_number_is_power_of_two(int number) {
+        var powerOfTwo = new PowerOfTwo(number);
+        boolean result = powerOfTwo.isPowerOfTwo();
+        assertThat(result).isTrue();
+    }
 }
