197ada080b17d0f127d3b4f91bdb067fb31988c5
ali <ali77erfagh99@gmail.com>
2024-10-23 14:45:05 +0330

    remove extra import

--- src/test/java/com/data_structure/bit/binery/LongestConsecutiveOneShould.java
+++ src/test/java/com/data_structure/bit/binery/LongestConsecutiveOneShould.java
@@ -1,10 +1,7 @@
 package com.data_structure.bit.binery;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 class LongestConsecutiveOneShould {
     @ParameterizedTest
