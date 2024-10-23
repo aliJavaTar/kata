3fc988ac8cfef79c6da32c61a17a29271a79b7c5
ali <ali77erfagh99@gmail.com>
2024-10-23 14:44:39 +0330

    solve it

--- src/main/java/com/data_structure/bit/binery/LongestConsecutiveOne.java
+++ src/main/java/com/data_structure/bit/binery/LongestConsecutiveOne.java
@@ -1,4 +1,22 @@
 package com.data_structure.bit.binery;
 
 public class LongestConsecutiveOne {
+    private int number;
+
+    public LongestConsecutiveOne(int number) {
+        this.number = number;
+    }
+
+    public int calculateMustSequenceOne() {
+        int count = 0;
+        int maxCount = 0;
+        while (number > 0) {
+            if ((number & 1) == 1) {
+                count++;
+                maxCount = Math.max(count, maxCount);
+            }
+            number >>= 1;
+        }
+        return maxCount;
+    }
 }
--- src/test/java/com/data_structure/bit/binery/LongestConsecutiveOneShould.java
+++ src/test/java/com/data_structure/bit/binery/LongestConsecutiveOneShould.java
@@ -1,7 +1,18 @@
 package com.data_structure.bit.binery;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LongestConsecutiveOneShould {
-
+    @ParameterizedTest
+    @ValueSource(ints = {3, 7, 15, 29})
+    void find_must_count_sequence_of_one(int input) {
+        var longestConsecutive = new LongestConsecutiveOne(input);
+        int result = longestConsecutive.calculateMustSequenceOne();
+//        int expected = expectedLongestConsecutiveOnes(input);
+//        assertThat(result).isEqualTo(expected);
+    }
 }
