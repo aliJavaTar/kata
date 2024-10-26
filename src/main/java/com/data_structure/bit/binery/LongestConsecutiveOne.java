ac4efda6cdb018abf24e3aba7384a2122e23b1f7
ali <ali77erfagh99@gmail.com>
2024-10-26 15:53:29 +0330

    refactor code and add new way

--- src/main/java/com/data_structure/bit/binery/LongestConsecutiveOne.java
+++ src/main/java/com/data_structure/bit/binery/LongestConsecutiveOne.java
@@ -12,7 +12,7 @@ public class LongestConsecutiveOne {
 
     public int calculateMustSequenceOne() {
         int count = 0;
-        while (number!=0) {
+        while (number != 0) {
             number = (number & (number << 1));
             count++;
         }
@@ -23,5 +23,20 @@ public class LongestConsecutiveOne {
     public int calculateMustSequenceOne_() {
         String[] array = Integer.toBinaryString(number).split(SPLIT_BY_ZERO);
         return Arrays.stream(array).mapToInt(String::length).max().orElse(0);
+    }
+
+    public int calculateMustSequenceOne__() {
+        int currentCount = 0;
+        int maxCount = 0;
+        while (number > 0) {
+            if ((number & 1) == 1) {
+                currentCount++;
+                maxCount = Math.max(maxCount, currentCount);
+            } else {
+                currentCount = 0;
+            }
+            number >>= 1;
+        }
+        return maxCount;
     }
 }
--- src/test/java/com/data_structure/bit/binery/LongestConsecutiveOneShould.java
+++ src/test/java/com/data_structure/bit/binery/LongestConsecutiveOneShould.java
@@ -9,7 +9,7 @@ class LongestConsecutiveOneShould {
     @CsvSource({"3 ,2 ", "7, 3", "15 , 4", "29,3", "5,1" ,"222,4"})
     void find_must_count_sequence_of_one(int input, int expected) {
         var longestConsecutive = new LongestConsecutiveOne(input);
-        int result = longestConsecutive.calculateMustSequenceOne();
+        int result = longestConsecutive.calculateMustSequenceOne__();
         Assertions.assertThat(result).isEqualTo(expected);
     }
 }
