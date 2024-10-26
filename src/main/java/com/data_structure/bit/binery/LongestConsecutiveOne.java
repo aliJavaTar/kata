51c3e892eec7579d68c5581ddc4b18ab0b8790b5
ali <ali77erfagh99@gmail.com>
2024-10-26 15:17:15 +0330

    add new logic and more test case

--- src/main/java/com/data_structure/bit/binery/LongestConsecutiveOne.java
+++ src/main/java/com/data_structure/bit/binery/LongestConsecutiveOne.java
@@ -1,5 +1,8 @@
 package com.data_structure.bit.binery;
 
+import java.io.InputStream;
+import java.util.stream.IntStream;
+
 public class LongestConsecutiveOne {
     private int number;
 
@@ -16,7 +19,23 @@ public class LongestConsecutiveOne {
                 maxCount = Math.max(count, maxCount);
             }
             number >>= 1;
+            count = 0;
         }
         return maxCount;
+    }
+
+    public int calculateMustSequenceOne_() {
+        int count = 0;
+        String binaryString = Integer.toBinaryString(number);
+        int max = 0;
+        for (int index = 0; index < binaryString.length(); index++) {
+            if (binaryString.charAt(index) == '1')
+                count++;
+            if (binaryString.charAt(index) == '0')
+                count = 0;
+
+            max = Math.max(count, max);
+        }
+        return max;
     }
 }
--- src/test/java/com/data_structure/bit/binery/LongestConsecutiveOneShould.java
+++ src/test/java/com/data_structure/bit/binery/LongestConsecutiveOneShould.java
@@ -1,15 +1,15 @@
 package com.data_structure.bit.binery;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class LongestConsecutiveOneShould {
     @ParameterizedTest
-    @ValueSource(ints = {3, 7, 15, 29})
-    void find_must_count_sequence_of_one(int input) {
+    @CsvSource({"3 ,2 ", "7, 3", "15 , 4", "29,3", "5,1" ,"222,4"})
+    void find_must_count_sequence_of_one(int input, int expected) {
         var longestConsecutive = new LongestConsecutiveOne(input);
-        int result = longestConsecutive.calculateMustSequenceOne();
-//        int expected = expectedLongestConsecutiveOnes(input);
-//        assertThat(result).isEqualTo(expected);
+        int result = longestConsecutive.calculateMustSequenceOne_();
+        Assertions.assertThat(result).isEqualTo(expected);
     }
 }
