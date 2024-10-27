f1352798b8a4ba57fea1b478232a62a07c5faee0
ali <ali77erfagh99@gmail.com>
2024-10-27 09:38:09 +0330

    solve  kata and pass test

--- src/main/java/com/data_structure/bit/binery/CountTotalSetBits.java
+++ src/main/java/com/data_structure/bit/binery/CountTotalSetBits.java
@@ -1,4 +1,23 @@
 package com.data_structure.bit.binery;
 
 public class CountTotalSetBits {
+    private int number;
+
+    public CountTotalSetBits(int number) {
+        this.number = number;
+    }
+
+    public int count() {
+        int count = 0;
+        int index = number;
+        while (number > 0) {
+            while (index > 0) {
+                count += index & 1;
+                index >>= 1;
+            }
+            number--;
+            index = number;
+        }
+        return count;
+    }
 }
--- src/test/java/com/data_structure/bit/binery/CountTotalSetBitsShould.java
+++ src/test/java/com/data_structure/bit/binery/CountTotalSetBitsShould.java
@@ -1,11 +1,17 @@
 package com.data_structure.bit.binery;
 
-import org.junit.jupiter.api.Test;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class CountTotalSetBitsShould {
 
-    @Test
-    void calculate_total_count_bits() {
-
+    //    @Test
+    @ParameterizedTest
+    @CsvSource({"1,1", "2,2", "3,4", " 4 ,5", "17,35"})
+    void calculate_total_count_bits(int input, int expected) {
+        var countTotalSetBits = new CountTotalSetBits(input);
+        int result = countTotalSetBits.count();
+        Assertions.assertThat(result).isEqualTo(expected);
     }
 }
