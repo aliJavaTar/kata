27a84130d6936fb1b518b727e443d2b3414a2fbe
ali <ali77erfagh99@gmail.com>
2024-10-27 09:49:09 +0330

    add new test case

--- src/test/java/com/data_structure/bit/binery/CountTotalSetBitsShould.java
+++ src/test/java/com/data_structure/bit/binery/CountTotalSetBitsShould.java
@@ -8,7 +8,7 @@ class CountTotalSetBitsShould {
 
     //    @Test
     @ParameterizedTest
-    @CsvSource({"1,1", "2,2", "3,4", " 4 ,5", "17,35"})
+    @CsvSource({"1,1", "2,2", "3,4", " 4 ,5", "17,35", "867,4100"})
     void calculate_total_count_bits(int input, int expected) {
         var countTotalSetBits = new CountTotalSetBits(input);
         int result = countTotalSetBits.count();
