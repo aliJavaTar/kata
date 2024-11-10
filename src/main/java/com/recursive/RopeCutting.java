2e9ee4e170b974117397fb611590f1e73550eada
ali <ali77erfagh99@gmail.com>
2024-11-10 11:57:25 +0330

    add new kata

--- src/main/java/com/recursive/RopeCutting.java
+++ src/main/java/com/recursive/RopeCutting.java
@@ -2,40 +2,24 @@ package com.recursive;
 
 public class RopeCutting {
 
-    public static int countOfPossible(int n, int a, int b, int c) {
 
-        if (n == 0)
-            return -1;
-        if(n <= -1)
-            return -1;
-        int res = Math.max(countOfPossible(n - a, a, b, c),
-                Math.max(countOfPossible(n - b, a, b, c),
-                        countOfPossible(n - c, a, b, c)));
-        if(res == -1)
-            return -1;
-        return res + 1;
 
-    }
-    public static void main(String [] args)
-    {
-        int n = 17, a = 10, b = 11, c = 3;
-
-        System.out.println(countOfPossible(n, a, b, c));
-
+    public int[] cutRodRecursive(int[] prices, int length) {
+        return null;
     }
 
-
-    public int cutRodRecursive(int[] prices, int length) {
-        if (length <= 0) {
-            return 0;
-        }
-
-        int maxRevenue = Integer.MIN_VALUE;
-        for (int i = 1; i <= length; i++) {
-            int revenue = prices[i] + cutRodRecursive(prices, length - i);
-            maxRevenue = Math.max(maxRevenue, revenue);
-        }
-
-        return maxRevenue;
-    }
 }
+//    public static int cutRodRecursive(int[] prices, int length) {
+//        if (length <= 0)
+//            return 0;
+//        int maxRevenue = 0;
+//        for (int index = 1; index < length; index++) {
+//            int revenue = prices[index] + cutRodRecursive(prices, length - index);
+//            maxRevenue = Math.max(maxRevenue, revenue);
+//        }
+//        return maxRevenue;
+//    }
+//    public static void main(String[] args) {
+//        int i = cutRodRecursive(new int[]{1, 5, 8, 9}, 4);
+//        System.out.println(i);
+//    }
--- src/test/java/com/recursive/RopeCuttingTest.java
+++ src/test/java/com/recursive/RopeCuttingTest.java
@@ -1,33 +1,29 @@
 package com.recursive;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class RopeCuttingTest {
 
     @ParameterizedTest
     @MethodSource("provideTestData")
-    void testRodCutting(int[] prices, int length, int expectedRevenue) {
+    void rodCutting(int[] prices, int length, int[] expectedRevenue) {
         var problem = new RopeCutting();
 
-        int actualRevenue = problem.cutRodRecursive(prices, length);
+        int[] result = problem.cutRodRecursive(prices, length);
 
-        assertNotEquals(-1, actualRevenue, "Actual revenue should not be -1");
-        assertEquals(expectedRevenue, actualRevenue, "Revenue calculation is incorrect");
-        assertTrue(prices.length >= 1 && prices.length <= length, "Prices array should be within bounds");
-        assertTrue(length <= 10, "Length should be within reasonable bounds");
-        assertEquals(expectedRevenue, actualRevenue);
+
+        Assertions.assertThat(result).isEqualTo(expectedRevenue);
     }
 
     private static Object[][] provideTestData() {
-        return new Object[][] {
-                {new int[]{1, 5, 8, 9}, 4, 10},
-                {new int[]{1, 5, 8, 9}, 3, 8},
-                {new int[]{1, 5, 8, 9}, 2, 5},
-                {new int[]{1, 5, 8, 9}, 1, 1},
-                {new int[]{1, 5, 8, 9}, 0, 0},
+        return new Object[][]{
+                {new int[]{1, 5, 8, 9}, 4, new int[]{2, 2, 10}},
+//                {new int[]{1, 5, 8, 9}, 3, 8},
+//                {new int[]{1, 5, 8, 9}, 2, 5},
+//                {new int[]{1, 5, 8, 9}, 1, 1},
+//                {new int[]{1, 5, 8, 9}, 0, 0},
 
         };
     }
