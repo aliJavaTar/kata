28cfe60a625302d7cc5dfe2a265347d20b2973e7
ali <ali77erfagh99@gmail.com>
2024-11-10 13:05:44 +0330

    add new kata

--- src/test/java/com/recursive/RopeCuttingTest.java
+++ src/test/java/com/recursive/RopeCuttingTest.java
@@ -4,27 +4,20 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import static org.assertj.core.api.Assertions.*;
+
 class RopeCuttingTest {
 
     @ParameterizedTest
     @MethodSource("provideTestData")
-    void rodCutting(int[] prices, int length, int[] expectedRevenue) {
+    void rodCutting(int[] prices, int expectedRevenue ) {
         var problem = new RopeCutting();
+        int result = problem.cutRodRecursive(prices);
 
-        int[] result = problem.cutRodRecursive(prices, length);
-
-
-        Assertions.assertThat(result).isEqualTo(expectedRevenue);
+        assertThat(result).isEqualTo(expectedRevenue);
     }
 
     private static Object[][] provideTestData() {
-        return new Object[][]{
-                {new int[]{1, 5, 8, 9}, 4, new int[]{2, 2, 10}},
-//                {new int[]{1, 5, 8, 9}, 3, 8},
-//                {new int[]{1, 5, 8, 9}, 2, 5},
-//                {new int[]{1, 5, 8, 9}, 1, 1},
-//                {new int[]{1, 5, 8, 9}, 0, 0},
-
-        };
+        return new Object[][]{{new int[]{1, 5}, 5},};
     }
 }
