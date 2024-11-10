e01acb85a4ac82b335f9e0f2c79917e803cceed5
ali <ali77erfagh99@gmail.com>
2024-11-10 10:45:29 +0330

    add kata with test

--- src/test/java/com/recursive/array/SumShould.java
+++ src/test/java/com/recursive/array/SumShould.java
@@ -3,13 +3,12 @@ package com.recursive.array;
 import org.junit.jupiter.api.Test;
 
 
-
 class SumShould {
 
     @Test
-    void calculate()
-    {
+    void calculate() {
         long[] array = {1, 2, 3, 4, 5, 6, 7, 8};
-        Sum.calculate_rev(array, 0);
+        var result = Sum.calculate_rev(array, 0);
+        System.out.println(result);
     }
 }
