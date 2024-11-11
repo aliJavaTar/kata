3be11362e52ec8f64b7e32b4850c00eae587d828
ali <ali77erfagh99@gmail.com>
2024-11-11 11:17:38 +0330

    add new kata

--- src/main/java/com/recursive/extra/Sum.java
+++ src/main/java/com/recursive/extra/Sum.java
@@ -1,14 +1,14 @@
 package com.recursive.extra;
 
 public class Sum {
-//    public String TextReceive(String text) {
-//        var empty = new StringBuilder();
-//
-//        for (int index = text.length() - 1; index >= 0; index--) {
-//            empty.append(text.charAt(index));
-//        }
-//        return empty.toString();
-//    }
+    public String TextReceive(String text) {
+        var empty = new StringBuilder();
+
+        for (int index = text.length() - 1; index >= 0; index--) {
+            empty.append(text.charAt(index));
+        }
+        return empty.toString();
+    }
 
     public String reverse(String text) {
         if (text.length() == 1)
--- src/test/java/com/recursive/array/SumShould.java
+++ src/test/java/com/recursive/array/SumShould.java
@@ -8,7 +8,7 @@ class SumShould {
     @Test
     void calculate() {
         long[] array = {1, 2, 3, 4, 5, 6, 7, 8};
-        var result = Sum.calculate_rev(array, 0);
-        System.out.println(result);
+//        var result = Sum.calculate_rev(array, 0);
+//        System.out.println(result);
     }
 }
