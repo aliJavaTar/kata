497899d161373d3e8635aee763549b38ccf9fe11
ali <ali77erfagh99@gmail.com>
2024-12-03 11:51:11 +0330

    add all fuc

--- src/main/java/com/func/PrTest.java
+++ src/main/java/com/func/PrTest.java
@@ -1,18 +1,29 @@
 package com.func;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
 
 public class PrTest {
     public static void main(String[] args) {
 
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+//       UnaryOperator;
+//        BinaryOperator;
 
-        Consumer<Integer> doubleConsumer = number -> System.out.println("Doubled value: " + (number * 2));
-        Consumer<Integer> printConsumer = number -> System.out.println("Original value: " + number);
-        Consumer<Integer> combinedConsumer = printConsumer.andThen(doubleConsumer);
-        numbers.forEach(combinedConsumer);
+//        mapper(number -> number * 2, List.of(1, 2, 3, 4)).forEach(System.out::println);
+
+
+//        FucGeneric<String, Integer> test = text -> text.length();
+//        System.out.println(test.execute("ali"));
+
+//        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+//
+//        Consumer<Integer> doubleConsumer = number -> System.out.println("Doubled value: " + (number * 2));
+//        Consumer<Integer> printConsumer = number -> System.out.println("Original value: " + number);
+//        Consumer<Integer> combinedConsumer = printConsumer.andThen(doubleConsumer);
+//        numbers.forEach(combinedConsumer);
 //        Function
 
         //primitive function
@@ -43,6 +54,18 @@ public class PrTest {
         ToIntBiFunction
          */
         /*
+        UnaryOperator
+        ToDoubleUnaryOperator
+        ToLongUnaryOperator
+        ToIntUnaryOperator
+         */
+        /*
+        Predicate
+        ToDoublePredicate
+        ToLongPredicate
+        ToIntPredicate
+         */
+        /*
         supplier
         IntSupplier
         BooleanSupplier
@@ -58,12 +81,29 @@ public class PrTest {
         DoubleConsumer
          */
 
-
 //        Player t = new Player("", 33);
 //        Predicate<Player> predicate = player -> player.countOfGolden() > 0;
 //        out.println(Predicate.not(predicate).test(t));
 
-    }
+//    }
+
+//    private static <T> List<T> mapper(UnaryOperator<Integer> operator, List<Integer> numbers) {
+//        List<Integer> number = new ArrayList<>();
+//        for (Integer i : numbers) {
+//            number.add(operator.apply(i));
+//        }
+//        return (List<T>) number;
+//    }
+//
+//    private static List<String> filterList(List<String> strings, Predicate<String> pri) {
+//        ArrayList<String> newString = new ArrayList<>();
+//
+//        for (String string : strings) {
+//            if (pri.test(string))
+//                newString.add(string);
+//        }
+//        return newString;
+//    }
 
 //    private static Supplier<Integer> convert(int number) {
 //        return () -> Math.abs(number);
@@ -72,4 +112,5 @@ public class PrTest {
 //    private static Function<Integer, String> convert(final String name) {
 //        return number -> (name + number);
 //    }
+    }
 }
