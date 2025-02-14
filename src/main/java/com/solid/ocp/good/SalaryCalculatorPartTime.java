032a3cd58ee40faf9bddcb42b7cd7983ee0a6e52
aliJavaTar <ali77erfagh99@gmail.com>
2025-02-14 16:45:52 +0330

    refactor code for clean coding

--- src/main/java/com/solid/ocp/good/SalaryCalculatorPartTime.java
+++ src/main/java/com/solid/ocp/good/SalaryCalculatorPartTime.java
@@ -3,8 +3,10 @@ package com.solid.ocp.good;
 public class SalaryCalculatorPartTime implements SalaryCalculator {
 
 
+    private static final int Half_Time = 2;
+
     @Override
     public double calculate(com.solid.ocp.good.Employee employee) {
-        return employee.getBaseSalary() / 2;
+        return employee.getBaseSalary() / Half_Time;
     }
 }
