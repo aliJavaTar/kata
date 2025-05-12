9b8961299a558baa505945ec198797cc555c0742
aliJavaTar <ali77erfagh99@gmail.com>
2025-05-12 09:54:16 +0330

    ADD : array kata

--- src/main/java/com/array/Sorted.java
+++ src/main/java/com/array/Sorted.java
@@ -2,4 +2,12 @@ package com.array;
 
 public class Sorted {
 
+    public boolean isSorted(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i - 1] > arr[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/test/java/com/array/SortedShould.java
+++ src/test/java/com/array/SortedShould.java
@@ -1,11 +1,34 @@
 package com.array;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class SortedShould {
 
     @Test
     void if_array_is_sorted_then_return_true() {
+        int[] array = {1, 2, 3, 4, 5};
+        Sorted sorted = new Sorted();
+        boolean isSorted = sorted.isSorted(array);
+        Assertions.assertTrue(isSorted);
+
+        int[] array1 = {1, 2, 5};
+        var sorted1 = new Sorted();
+        boolean isSorted1 = sorted1.isSorted(array1);
+        Assertions.assertTrue(isSorted1);
+    }
+
+    @Test
+    void if_array_is_not_sorted_then_return_false() {
+        int[] array = {3, 2, 5};
+        Sorted sorted = new Sorted();
+        boolean isSorted = sorted.isSorted(array);
+        Assertions.assertFalse(isSorted);
+
+        int[] array1 = {1, 2, 2, 1};
+        var sorted1 = new Sorted();
+        boolean isSorted1 = sorted1.isSorted(array1);
+        Assertions.assertFalse(isSorted1);
 
     }
 }
