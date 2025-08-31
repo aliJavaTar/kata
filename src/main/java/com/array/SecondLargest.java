1489dc0a95d9ee5628fa9249cb65d5efde40b78b
aliJavaTar <ali77erfagh99@gmail.com>
2025-08-31 15:39:27 +0330

    add new kata

--- src/main/java/com/array/SecondLargest.java
+++ src/main/java/com/array/SecondLargest.java
@@ -25,9 +25,31 @@ public class SecondLargest {
         return second;
     }
 
+    public static int secondLargest(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return -1;
+        }
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+        int third = Integer.MIN_VALUE;
+        
+        for (int num : arr) {
+            if (num > first) {
+                first = num;
+            } else if (num != first && num > second) {
+                second = num;
+            } else if (num != first && num != second && num > third) {
+                third = num;
+            }
+        }
+        return third;
+    }
+
 
     public static void main(String[] args) {
-        int[] arr = {10, 5, 10};
+        int[] arr = {12, 5, 10, 7};
         System.out.println("Second largest: " + getSecondLargest(arr));
+        System.out.println("Second largest: " + secondLargest(new int[]{12, 5, 10}));
     }
+
 }
---
+++ src/main/java/com/integer/SingleNumber.java
@@ -0,0 +1,20 @@
+package com.integer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SingleNumber {
+
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey();
+            }
+        }
+        return -1;
+    }
+}
